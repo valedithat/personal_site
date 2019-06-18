87890c08f4649d13443ed12413711bf19f60646d
Valerie Trudell <valedithat@pm.me>
2019-06-18 19:21:21 +0000

    migrated articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_191638) do
+ActiveRecord::Schema.define(version: 2019_06_18_191827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,4 +34,13 @@ ActiveRecord::Schema.define(version: 2019_06_18_191638) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_categories_on_article_id"
+  end
+
+  add_foreign_key "categories", "articles"
 end
