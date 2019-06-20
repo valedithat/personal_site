f0716685e509eabdc991719fc306704cc3498a20
Valerie Trudell <valedithat@pm.me>
2019-06-20 17:32:50 +0000

    updates to admin, category and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_170914) do
+ActiveRecord::Schema.define(version: 2019_06_20_172934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_170914) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -32,12 +33,16 @@ ActiveRecord::Schema.define(version: 2019_06_20_170914) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description", null: false
+    t.bigint "admin_id", null: false
+    t.index ["admin_id"], name: "index_articles_on_admin_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "admins"
 end
