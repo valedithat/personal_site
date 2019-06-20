513de0846f98fb260c782bdebfcebc6506d421e5
Valerie Trudell <valedithat@pm.me>
2019-06-20 16:03:57 +0000

    remove directory so I can get work done, todo: return to dashboard_controller #show and #welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/show'
+  get 'articles/index'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'dashboard#welcome'
+  # root 'dashboard#show'
+  resources :dashboard, only: [:show]
+  resources :articles
 end
