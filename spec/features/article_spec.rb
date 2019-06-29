e74b04b11829cd907cf7bee3b7cf486cc89575fd
Valerie Trudell <valedithat@pm.me>
2019-06-29 17:15:50 +0000

    fixed article show tests and cleaned up fixtures

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Article' do
   let(:user) { users(:one) }
 
-  describe "GET index" do
+  describe 'GET index' do
     let(:path) { articles_path }
 
     it 'shows all articles' do
@@ -11,6 +11,35 @@ describe 'Article' do
       Article.all.each do |article|
         expect(page).to have_content article.title
         expect(page).to have_content article.description
+      end
+    end
+  end
+
+  describe 'GET show' do
+    let(:article) { articles(:one) }
+    let(:path) { article_path(article) }
+
+    before { visit path }
+
+    it 'displays article title' do
+      expect(page).to have_content article.title
+    end
+
+    it 'displays article date' do
+      expect(page).to have_content article.updated_at
+    end
+
+    it 'displays article author' do
+      expect(page).to have_content article.user.name
+    end
+
+    it 'displays article body' do
+      expect(page).to have_content article.body
+    end
+
+    xit 'displays article categories' do
+      article.categories.each do |category|
+        expect(page).to have_content category.name
       end
     end
   end
