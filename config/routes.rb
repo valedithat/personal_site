f169f0d2160df7048e0949ba6111f5075303f903
Valerie Trudell <valedithat@pm.me>
2019-06-29 22:41:03 +0000

    moved devise views to user, and moved all devise routes to user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root 'dashboard#show'
-  resources :dashboard, only: [:show]
   resources :articles
 end
