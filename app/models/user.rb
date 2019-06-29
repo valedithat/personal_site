39942ba0a57dad8e6b9957d815464148066fe755
Valerie Trudell <valedithat@pm.me>
2019-06-29 18:11:07 +0000

    add tests for user relationship and clean up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+
   # omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable, :lockable, :timeoutable
