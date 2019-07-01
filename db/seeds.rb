6dca7d333df5d8a98c75f48992194ff8c12dc930
Valerie Trudell <valedithat@pm.me>
2019-07-01 03:55:45 +0000

    add active_admin_comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Valerie Trudell", email: "valedithat@pm.me", password: "DmuaCa2TTw4WNwX43KRuiqqHagasqaYG4YXTCenTpe6Wv5QwE", password_confirmation: "DmuaCa2TTw4WNwX43KRuiqqHagasqaYG4YXTCenTpe6Wv5QwE") if Rails.env.development?
