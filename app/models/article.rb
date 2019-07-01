609a7d6dca5d5ce476f8c3556d5a0209b269d3d7
Valerie Trudell <valedithat@pm.me>
2019-07-01 03:58:31 +0000

    allow categories to be created on active admin articles new page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  attr_accessor :category_attributes
+
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
+
+  accepts_nested_attributes_for :categories, reject_if: :all_black
 end
