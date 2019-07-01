fdb9ded06ab4a6cf0224cad87d2a03037520c5d1
Valerie Trudell <valedithat@pm.me>
2019-07-01 03:56:39 +0000

    allow active admin to handle routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    confirmations: 'users/confirmations',
-    unlocks: 'users/unlocks'
-  }
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # devise_for :users, controllers: {
+  #   # all: 'admin'
+  #   # sessions: 'users/sessions',
+  #   # passwords: 'users/passwords',
+  #   # registrations: 'users/registrations',
+  #   # confirmations: 'users/confirmations',
+  #   # unlocks: 'users/unlocks'
+  # }
 
   resources :articles
 end
