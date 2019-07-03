b7c73fc9c7d6440a654915d819c06b72cc3fdf7b
Valerie Trudell <valedithat@pm.me>
2019-07-03 22:45:00 +0000

    make /articles route for the time being

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # devise_for :users, controllers: {
-  #   # all: 'admin'
-  #   # sessions: 'users/sessions',
-  #   # passwords: 'users/passwords',
-  #   # registrations: 'users/registrations',
-  #   # confirmations: 'users/confirmations',
-  #   # unlocks: 'users/unlocks'
-  # }
 
   resources :articles
 end
