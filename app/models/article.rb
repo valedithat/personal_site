d8d9398a07b7bc187e968b8c964233742a7220d1
Valerie Trudell <valedithat@pm.me>
2019-07-20 14:52:16 +0000

    add easy_dates and remove logic for nested attributes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ApplicationRecord
-  attr_accessor :category_attributes
-
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
 
-  accepts_nested_attributes_for :categories, reject_if: :all_black
+  define_easy_dates do
+    format_for :created_at, :format => "%A, %B %-d, %Y at%l:%M%P"
+    format_for :updated_at, :format => "%A, %B %-d, %Y at%l:%M%P"
+  end
 end
