b20ab520bd06ddd0e6ae497e1d94b744348a0c4b
Valerie Trudell <valedithat@pm.me>
2019-10-07 15:37:07 +0000

    add project model with easy dates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  belongs_to :user
+
+  define_easy_dates do
+    format_for :created_at, :format => "%A, %B %-d, %Y at%l:%M%P"
+    format_for :updated_at, :format => "%A, %B %-d, %Y at%l:%M%P"
+  end
 end
