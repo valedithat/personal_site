3cf98c343044e544ad4d81296f9b45c9125cff2b
Valerie Trudell <valedithat@pm.me>
2019-10-09 15:31:02 +0000

    add user reference to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_222154) do
+ActiveRecord::Schema.define(version: 2019_10_08_055458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 2019_07_24_222154) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -122,4 +124,5 @@ ActiveRecord::Schema.define(version: 2019_07_24_222154) do
   add_foreign_key "article_categories", "articles"
   add_foreign_key "article_categories", "categories"
   add_foreign_key "articles", "users"
+  add_foreign_key "projects", "users"
 end
